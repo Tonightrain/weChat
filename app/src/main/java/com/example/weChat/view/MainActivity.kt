9ff0dc430b2eab7add57bc2bf6f8ec2152eb2169
tonightrain <805560812@qq.com>
2020-11-09 14:48:33 +0800

    [feat]:Pull down to refresh Pull up to load

--- app/src/main/java/com/example/weChat/view/MainActivity.kt
+++ app/src/main/java/com/example/weChat/view/MainActivity.kt
@@ -1,25 +1,38 @@
 package com.example.weChat.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.Glide
 import com.example.weChat.R
 import com.example.weChat.adapter.MomentsAdapter
+import com.example.weChat.model.Moment
 import com.example.weChat.util.MyApplication
 import com.example.weChat.util.MyApplication.Companion.context
 import com.example.weChat.viewModel.WeChatViewModel
 import com.example.weChat.viewModel.WeChatViewModelFactory
 import com.google.gson.GsonBuilder
+import kotlinx.coroutines.*
+import org.w3c.dom.Text
+import kotlin.math.ceil
 
+
+private const val FIRST_NUMBER = 5
+private const val SECOND_NUMBER = 10
+private const val LAST_NUMBER = 15
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +40,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var profileAvatar: ImageView
     private lateinit var profileNick: TextView
     private lateinit var weChatViewModel: WeChatViewModel
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var sliceMomentData: List<Moment>
+    private lateinit var nestedView: NestedScrollView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,12 +52,15 @@ class MainActivity : AppCompatActivity() {
         initView()
         setProfileData()
         setMomentsData()
+
     }
 
     private fun initView() {
         profileImage = findViewById(R.id.profile_image)
         profileAvatar = findViewById(R.id.profile_avatar)
         profileNick = findViewById(R.id.profile_nick)
+        swipeRefresh = findViewById(R.id.swipeRefreshLayout)
+        nestedView = findViewById(R.id.nestedScrollView)
 
         weChatViewModel =
             ViewModelProvider(this, WeChatViewModelFactory()).get(WeChatViewModel::class.java)
@@ -59,16 +79,57 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun setMomentsData() {
         weChatViewModel.loadMomentsData()
         weChatViewModel.momentsData.observe(this, { momentsData ->
 
-            val momentsView = findViewById<View>(R.id.moments) as RecyclerView
-            val adapterMoment = MomentsAdapter(momentsData)
-            momentsView.adapter = adapterMoment
-            momentsView.layoutManager = LinearLayoutManager(context)
+            setViewData(momentsData, listOf(0, 1, 2, 3, 4))
+
+            swipeRefresh.setOnRefreshListener {
+                setViewData(momentsData, listOf(0, 1, 2, 3, 4))
+                swipeRefresh.isRefreshing = false
+            }
+
+            nestedView.setOnScrollChangeListener { scrollview: NestedScrollView, _: Int, y: Int, _: Int, _: Int ->
+                if (y == (scrollview.getChildAt(0).measuredHeight) - scrollview.measuredHeight) {
+
+                    when (sliceMomentData.size) {
+                        FIRST_NUMBER -> {
+                            setViewData(momentsData, listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+                            Toast.makeText(this, R.string.load_message, Toast.LENGTH_SHORT).show()
+                        }
+
+                        SECOND_NUMBER -> {
+                            setViewData(
+                                momentsData, listOf(
+                                    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+                                )
+                            )
+                            Toast.makeText(this, R.string.load_message, Toast.LENGTH_SHORT).show()
+                        }
+                        LAST_NUMBER -> {
+                            setMomentsDataAdapter(momentsData)
+                            Toast.makeText(this, R.string.load_message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
         })
     }
+
+    private fun setMomentsDataAdapter(data: List<Moment>) {
+        val momentsView = findViewById<View>(R.id.moments) as RecyclerView
+        val adapterMoment = MomentsAdapter(data)
+        momentsView.adapter = adapterMoment
+        momentsView.layoutManager = LinearLayoutManager(context)
+    }
+
+    private fun setViewData(momentsData: ArrayList<Moment>, list: List<Int>) {
+        sliceMomentData = momentsData.slice(list)
+        setMomentsDataAdapter(sliceMomentData)
+    }
+
 }
 
 
